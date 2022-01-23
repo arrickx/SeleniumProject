341d6d0d6bca4810f7f7d6a730c84e85318ec762
Richter Lau <ar.rick@hotmail.com>
2022-01-23 17:03:29 -0500

    Initialize the project

---
+++ Selenium_EssT/Exercise Files/02_03/start/test/Autocomplete.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/richterlau/Downloads/Selenium_EssT/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("2154 Hylan Blvd, Staten Island, NY 10306");
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
---
+++ Selenium_EssT/Exercise Files/06_01/start/test/Form.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/richterlau/Downloads/Selenium_EssT/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("First");
+        driver.findElement(By.id("last-name")).sendKeys("Last");
+        driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
+        driver.findElement(By.id("radio-button-2")).click();
+        driver.findElement(By.id("checkbox-1")).click();
+        driver.findElement(By.cssSelector("option[value='1']")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("01/01/2022");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
